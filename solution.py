3e1c12f1b2df57214b58e2486585144b02f05612
kg2879 <71200683+kg2879@users.noreply.github.com>
2021-02-14 22:50:15 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ def webServer(port=13331):
     while True:
         #Establish the connection
         #print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept()#Fill in start      #Fill in end
+        connectionSocket,addr = serverSocket.accept()#Fill in start      #Fill in end
         try:
             message = connectionSocket.recv(1024)#Fill in start    #Fill in end
             #print(message)
